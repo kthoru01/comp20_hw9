5163c23a6be6fd53037cf6c9ab40fcb82b5ec91a
kthoru01 <59969455+kthoru01@users.noreply.github.com>
2020-03-30 20:58:48 -0400

    Add files via upload

---
+++ songs_java.js
@@ -0,0 +1,71 @@
+var songs = [
+	{
+	"title"   : "Say You Won't Let Go",
+	"artists" : ["James Arthur"],
+	"genres"  : ["pop"],
+	"year"    : 2016,
+	},
+
+	{
+	"title"   : "Falling Like the Stars",
+	"artists" : ["James Arthur"],
+	"genres"  : ["pop"],
+	"year"    : 2019,
+	},
+
+	{
+	"title"   : "Tennessee Whiskey",
+	"artists" : ["Chris Stapleton"],
+	"genres"  : ["country"],
+	"year"    : 2015,
+	},
+
+	{
+	"title"   : "In Case You Didn't Know",
+	"artists" : ["Brett Young"],
+	"genres"  : ["country"],
+	"year"    : 2017,
+	},
+
+	{
+	"title"   : "10,000 Hours",
+	"artists" : ["Dan and Shay", "Justin Bieber"],
+	"genres"  : ["country"],
+	"year"    : 2019,
+	},
+
+	{
+	"title"   : "Beautiful People",
+	"artists" : ["Ed Sheeran"],
+	"genres"  : ["pop"],
+	"year"    : 2019,
+	},
+
+	{
+	"title"   : "Isn't She Lovely",
+	"artists" : ["Stevie Wonder"],
+	"genres"  : ["R&B", "soul"],
+	"year"    : 1976,
+	},
+
+	{
+	"title"   : "Sir Duke",
+	"artists" : ["Stevie Wonder"],
+	"genres"  : ["R&B"],
+	"year"    : 1976,
+	},
+
+	{
+	"title"   : "Superstition",
+	"artists" : ["Stevie Wonder"],
+	"genres"  : ["R&B", "soul"],
+	"year"    : 1972,
+	},
+
+	{
+	"title"   : "Signed Sealed Delivered",
+	"artists" : ["Stevie Wonder"],
+	"genres"  : ["soul"],
+	"year"    : 1970,
+	}
+];
